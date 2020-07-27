a182ca2bf06ca124076902a77ab182ae341a95b6
root <root@FrazileGamer.localdomain>
2020-07-27 19:25:57 +0530

    download all pages

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ CONFIG = {
     'resolution': "1080x1920",  # depends upon your screen resolution :default 1920x1080
     'foldername': "dark",  # folder in which you want to download Wallpapers
     # pages to download in range, if 0 it will download all pages available :default 1-3
-    'pages_to_download': '1-1',
+    'pages_to_download': '0',
 }
